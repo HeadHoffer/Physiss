dc00ac444340833e6a70834ce525e271e065059d
Karjalainen Kim TTV15SP <kimkarjalainen@kamk.fi>
2020-01-30 14:47:09 +0200

    Main.cpp refucktored, collision refucktored and is much better now

--- Physiss/Physiss/include/Collision.h
+++ Physiss/Physiss/include/Collision.h
@@ -11,16 +11,15 @@ public:
 	Collision();
 	~Collision();
 
+	void HandleCollision(std::vector<sf::RectangleShape> rects, std::vector<sf::CircleShape> circles);
 
 	bool CheckCollision(sf::RectangleShape rec1, sf::RectangleShape rec2);
-
-
 	bool CheckCollision(sf::CircleShape cir, sf::RectangleShape rec);
-
 	bool CheckCollision(sf::CircleShape cir1, sf::CircleShape cir2);
 
+	void CollisionImpulse(sf::CircleShape cir1, sf::CircleShape cir2);
+
 	float Distance(sf::Vector2f pos1, sf::Vector2f pos2);
+	sf::Vector2f VectorDistance(sf::Vector2f pos1, sf::Vector2f pos2);
 	
 };
-
-//TODO: Collsion
--- Physiss/Physiss/source/Source.cpp
+++ Physiss/Physiss/source/Source.cpp
@@ -1,94 +1,9 @@
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include <SFML/System.hpp>
-#include "CubeGenerator.h"
-#include "Collision.h"
-#include <iostream>
+#include "Window.h"
 
 int main()
 {
-	CubeGenerator cg;
-	Collision col;
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Sometimes it doesn't just work");
-	std::vector<sf::RectangleShape> arr;
-	std::vector<sf::CircleShape> circles;
-
-	float circleWidth = 30.f;
-	float rectHeight = 150;
-	std::vector<sf::RectangleShape> hitBoxes;
-	std::vector<sf::CircleShape> hitBoxCorners;
-	std::cout << "Give square height: ";
-	std::cin >> rectHeight;
-	std::cout << "\nGive circle radius: ";
-	std::cin >> circleWidth;
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-				window.close();
-
-			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-				sf::Vector2i mPos = sf::Mouse::getPosition(window);
-				sf::RectangleShape newCube = cg.NewCube(mPos, rectHeight);
-				std::vector<sf::CircleShape> corners = cg.CubeHitCircle(newCube, circleWidth);
-				std::vector<sf::RectangleShape> boxes = cg.CubeHitbox(newCube, circleWidth);
-				hitBoxCorners.insert(hitBoxCorners.end(), corners.begin(), corners.end());
-				hitBoxes.insert(hitBoxes.end(), boxes.begin(), boxes.end());
-				arr.push_back(newCube);
-			}
-
-			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right) {
-				sf::Vector2i mPos = sf::Mouse::getPosition(window);
-				sf::CircleShape newCircle = cg.NewCircle(mPos, circleWidth);
-				circles.push_back(newCircle);
-			}
-		}
-
-		if ((int)arr.size() > 0) {
-			for (auto x = arr.begin(); x != arr.end(); x++) {
-				for (auto y = arr.begin(); y != arr.end(); y++) {
-					if (x != y && col.CheckCollision(*x, *y))
-						//std::cout << "Collision happened\n";
-						continue;
-				}
-			}
-		}
-		if ((int)circles.size() > 0) {
-			for (auto z = circles.begin(); z != circles.end(); z++) {
-				for (auto z2 = circles.begin(); z2 != circles.end(); z2++) {
-					if (z != z2 && col.CheckCollision(*z, *z2))
-						//std::cout << "Circle collision happened\n";
-						continue;
-				}
-			}
-		}
-		if ((int)circles.size() > 0 && (int)arr.size() > 0) {
-			for (auto r = arr.begin(); r != arr.end(); r++) {
-				for (auto c = circles.begin(); c != circles.end(); c++) {
-					if (col.CheckCollision(*c, *r))
-						std::cout << "Circle has met rectangle\n";
-				}
-			}
-		}
-		window.clear();
-		for (int i = 0; i < (int)hitBoxes.size(); ++i) {
-			window.draw(hitBoxes[i]);
-		}
-		for (int i = 0; i < (int)hitBoxCorners.size(); ++i) {
-			window.draw(hitBoxCorners[i]);
-		}
-		for (int i = 0; i < (int)arr.size(); ++i) {
-			window.draw(arr[i]);
-		}
-		for (int i = 0; i < (int)circles.size(); ++i) {
-			window.draw(circles[i]);
-		}
-
-		window.display();
-	}
+	Window window;
+	window.RenderWindow();
 
 	return 0;
 }
